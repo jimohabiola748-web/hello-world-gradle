813b843a08efea58a6338f1e3fba430e8ea6d18e
jimohabiola748-web <jimohabiola748@gmail.com>
2026-09-12 22:07:19 -0400

    Push the changes

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Hello Pragra!";
+        return "Hello metro!";
     }
 
     public static void main(String[] args) {
